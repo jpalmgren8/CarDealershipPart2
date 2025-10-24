6b00c0e7f4cb297ee970e45687e548e926541c94
Julian Palmgren <julianpalmgren@gmail.com>
2025-10-24 13:47:27 -0400

    Commit #2: 10/14/25
    Worked on the following:
    - Finished phase 2 of the car dealership workshop
    - Imported file reader and buffered reader
    - Made a getDealerShip method for reading in the inventory.csv
    - Made an empty saveDealership method for saving to file later
    
    Still need to work on:
    - Phase 3, user interface layer
    - All methods for UI class
    - saveDealership method for saving to file
    
    Possible changes required:
    - Directions say each search method in the dealership need to return null
    - Currently I have it returning a list of search results
    - May need to change how the vehicle array list is named

--- src/main/java/com/pluralsight/DealershipFileManager.java
+++ src/main/java/com/pluralsight/DealershipFileManager.java
@@ -1,4 +1,31 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+
 public class DealershipFileManager {
+
+    public void getDealership() {
+
+        try {
+            FileReader fileReader = new FileReader("src/main/resources/inventory.csv");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String line;
+
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Couldn't read file, please try again!");
+        }
+    }
+
+    public void saveDealership() {
+
+    }
+
 }
